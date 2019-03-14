115f1005a3e31befe67184bdefddb487fac9111f
荻野 <gieno1975@extra.ocn.ne.jp>
2019-03-14 21:38:21 +0900

    プロジェクト ファイルを追加します。

---
+++ 2in1Viewer/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace _2in1Viewer.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
